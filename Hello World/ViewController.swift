f62fec9ff2d5a2f5f4484e62d40371d719338d9b
Andrey Sinetskiy <siniy.85@mail.ru>
2021-08-17 19:36:41 +0300

    Work With Logic

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldlabel: UILabel!
+    @IBOutlet weak var showtextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldlabel.isHidden = true
+        helloWorldlabel.textColor = .systemYellow
+        showtextButton.layer.cornerRadius = 10
         
     }
 
-
+    @IBAction func showTextButtonPressed() {
+        helloWorldlabel.isHidden.toggle()
+        
+        if helloWorldlabel.isHidden {
+            showtextButton.setTitle("Show Text", for: .normal)
+        } else {
+            showtextButton.setTitle("Hide Text", for: .normal)
+        }
+    }
+    
 }
 
